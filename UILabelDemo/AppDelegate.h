86e3a5e66c631f9f9f83dcc8f3a06f3f794431cd
buaa0300 <buaa0300@163.com>
2016-09-12 10:50:21 +0800

    Initial Commit

---
+++ UILabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UILabelDemo
+//
+//  Created by zhongpingjiang on 16/9/12.
+//  Copyright © 2016年 shaoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UILabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILabelDemo
+//
+//  Created by zhongpingjiang on 16/9/12.
+//  Copyright © 2016年 shaoqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
